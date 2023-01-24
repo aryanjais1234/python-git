baab0e5068b8bd3b0000098b45f22df3e4013595
aryanjais1234 <aryanjais1234@gmail.com>
2023-01-25 01:28:50 +0530

    new files added

---
+++ main.py
@@ -0,0 +1,6 @@
+import calculator
+
+print(calculator.add(1,2))
+print(calculator.sub(1,2))
+print(calculator.mul(1,2))
+print(calculator.div(1,2))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+print(calculator.mul(1,2))
+# print(calculator.div(1,2))
